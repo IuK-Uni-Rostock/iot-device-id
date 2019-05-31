ccd04e1b2a3c6117733bb37937221c82dd84e28d
Johann Bauer <bauerj@bauerj.eu>
2019-05-31 10:16:59 +0200

    Add ARP discovery

---
+++ lib/discovery/arp.py
@@ -0,0 +1,26 @@
+import asyncio
+import logging
+
+from mac_vendor_lookup import AsyncMacLookup
+
+from lib.utils import get_arp_table
+
+
+async def start(on_receive):
+    mac = AsyncMacLookup()
+    while True:
+        arp_table = await get_arp_table()
+        for elem in arp_table:
+            try:
+                vendor = await mac.lookup(elem[1])
+                on_receive(elem[0], "MAC", vendor)
+                logging.info("Vendor for {} is {}".format(elem[0], vendor))
+            except KeyError:
+                pass
+        await asyncio.sleep(30)
+
+
+if __name__ == "__main__":
+    loop = asyncio.ProactorEventLoop()
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(start(None))
