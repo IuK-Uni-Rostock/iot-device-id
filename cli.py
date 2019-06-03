9ecac67dcd8235088f9889dfc65626766f1e1010
Johann Bauer <bauerj@bauerj.eu>
2019-06-03 18:00:23 +0200

    Sort table entries

--- cli.py
+++ cli.py
@@ -8,14 +8,23 @@ class ConsoleUI(BaseUI):
     def __init__(self):
         super().__init__()
         self.tt = None
+        self.rows = []
+        self.sort_by = 0
 
     def set_headers(self, headers):
         self.tt = TexttableWithLogStream(log_stream, headers)
 
+    def sort_by_row(self, i):
+        self.sort_by = i
+
     def add_row(self, values):
-        self.tt.add_row(values)
+        self.rows.append(values)
 
     def draw(self):
+        rows = sorted(self.rows, key=lambda x: int(x[self.sort_by][:-1]), reverse=True)
+        self.rows = []
+        for v in rows:
+            self.tt.add_row(v)
         self.tt.draw()
 
     def record(self, ip, name):
