a9cc3a813500ecd41a9da57ec701577ae7772709
Johann Bauer <bauerj@bauerj.eu>
2019-06-21 14:28:43 +0200

    Fix SSDP discovery

--- lib/discovery/ssdp.py
+++ lib/discovery/ssdp.py
@@ -1,24 +1,30 @@
-import asyncio
 import socket
+import struct
 import logging
 
-import ssdp
+SSDP_BROADCAST_PORT = 1900
+SSDP_BROADCAST_ADDR = "239.255.255.250"
 
-enabled = True
 
+async def start(loop, on_receive):
+    class SSDPProtocol(object):
+        bad_prefixes = ["uuid:"]
+        bad_fields = ["LOCATION", "HOST", "CACHE-CONTROL"]
 
-async def start(parent):
-    class MyProtocol(ssdp.SimpleServiceDiscoveryProtocol):
+        # noinspection PyMethodMayBeStatic
+        def datagram_received(self, data, addr):
+            fields = [v.split(": ") for v in data.decode("utf8").splitlines()]
+            method, fields = fields[0][0], fields[1:]
+            fields = [v for v in fields if len(v) > 1 and not v[1].startswith("uuid:")]
+            for item in fields:
+                on_receive(addr[0], "SSDP", "{}: {}".format(method, item))
 
-        def response_received(self, response, addr):
-            parent.on_receive(addr, "SSDP", response)
+    addrinfo = socket.getaddrinfo(SSDP_BROADCAST_ADDR, None)[0]
+    sock = socket.socket(addrinfo[0], socket.SOCK_DGRAM)
+    group_bin = socket.inet_pton(addrinfo[0], addrinfo[4][0])
+    sock.bind(('', SSDP_BROADCAST_PORT))
+    mreq = group_bin + struct.pack('=I', socket.INADDR_ANY)
+    sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
 
-        def request_received(self, request, addr):
-            logging.debug("SSDP Request for {} from {}".format(request, addr))
-
-    loop = asyncio.get_event_loop()
-    transport, protocol = await loop.create_datagram_endpoint(MyProtocol, family=socket.AF_INET)
+    await loop.create_datagram_endpoint(SSDPProtocol, sock=sock)
     logging.debug("Listening for SSDP announcements")
-
-    notify = ssdp.SSDPRequest('M-SEARCH', headers=[("ST", "ssdp:all"), ("MAN", "ssdp:discover")])
-    notify.sendto(transport, (MyProtocol.MULTICAST_ADDRESS, 1982))
